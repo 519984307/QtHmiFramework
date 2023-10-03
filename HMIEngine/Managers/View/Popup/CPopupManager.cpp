239e14df3e2ebf27d03fa0b5febb592050af0c15
Tran Phuoc Tin <tptin.76@gmail.com>
2023-10-04 02:32:05 +0700

    u

--- HMIEngine/Managers/View/Popup/CPopupManager.cpp
+++ HMIEngine/Managers/View/Popup/CPopupManager.cpp
@@ -1,17 +1,20 @@
 #include "CPopupManager.h"
 
 CPopupManager::CPopupManager()
-{}
+{
+}
 
 CPopupManager::~CPopupManager()
 {
-
 }
 
 void CPopupManager::attach(const S_VIEW_INFORMATION *info)
 {
     CPP_LOG_DEBUG("Load POPUP from path [%s]", info->path);
-    // emit signalLoadQml(info, m_load_qml_cb);
+
+    m_load_qml_payload.info = info;
+    m_load_qml_payload.cb = &m_load_qml_cb;
+    CEventManager::instance()->dispatchEvent(E_EVENT_LOAD_QML, &m_load_qml_payload);
 }
 
 void CPopupManager::detach(const S_VIEW_INFORMATION *info)
@@ -24,9 +27,10 @@ void CPopupManager::loadNewQmFromCallback(CView *view)
 {
     CPP_LOG_DEBUG("[Entry]")
 
-    if(view == nullptr) return;
+    if (view == nullptr)
+        return;
 
-    CPopup* next = (CPopup*)view;
+    CPopup *next = (CPopup *)view;
     next->show();
 
     m_view = next;
